c9b2a43e1844f2bc88f2eb5839c095b1aaf66e3a
Alexander Gounares <alex@gounares.com>
2023-02-25 16:03:56 -0800

    add load file menu

--- src/extension.ts
+++ src/extension.ts
@@ -30,4 +30,31 @@ export function activate(context: vscode.ExtensionContext) {
 		),
 		new BoostKernel()
 	);
+	// Create a new status bar item with a button
+	const loadCodeFileButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+	loadCodeFileButton.text = "$(file-directory) Load Code File";
+	loadCodeFileButton.command = 'polyverse-boost-notebook.loadCodeFile';
+	loadCodeFileButton.show();
+
+	// Register a command to handle the button click
+	context.subscriptions.push(vscode.commands.registerCommand('polyverse-boost-notebook.loadCodeFile', async () => {
+		// Use the vscode.window.showOpenDialog method to let the user select a file
+		const fileUri = await vscode.window.showOpenDialog({
+			canSelectMany: false,
+			openLabel: 'Load Code File',
+			filters: {
+				'All Files': ['*']
+			}
+		});
+
+		if (fileUri && fileUri[0]) {
+			// Use the vscode.workspace.fs.readFile method to read the contents of the file
+			const fileContents = await vscode.workspace.fs.readFile(fileUri[0]);
+
+			// Create a new cell with the contents of the file
+			const language = 'typescript';
+			const cell = new vscode.NotebookCellData(vscode.NotebookCellKind.Code, fileContents.toString(), language);
+
+		}
+	}));
 }
