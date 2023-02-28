63cb0996d5f1d2fd7c6992e5742467d6a6eb9455
Alexander Gounares <alex@gounares.com>
2023-02-28 11:31:30 -0800

    get the first version working

--- src/controller.ts
+++ src/controller.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import axios from 'axios';
 
 export class BoostKernel {
 	private readonly _id = 'polyverse-boost-notebook-kernel';
@@ -38,11 +39,32 @@ export class BoostKernel {
 		try {
 			//make an array of CellOputputItems with the type NotebookCellOutputItem
 
+			// get the code from the cell
+			const code = cell.document.getText();
+
+			// using axios, make a web POST call to localhost:8080/explain with the code as in a json object code=code
+			const response = await axios.post('http://localhost:8080/explain', { code: code });
+
+
+			const summarydata = response.data;
+			// print the response to the console
+			console.log(summarydata);
+
+			// now take the summary and using axios send it to localhost:8080/generate/python with the summary in a json object summary=summary
+			const response2 = await axios.post('http://localhost:8080/generate/python', { explanation: summarydata.explanation });
+
+			const generatedCode = await response2.data;
+
+			// print the response to the console
+			console.log(generatedCode);
+
+
+
 			const outputItems: vscode.NotebookCellOutputItem[] = [];
 			// push a new CellOutputItem with the json parsed text of the cell with the markdown string of "this is a test"
-			outputItems.push(vscode.NotebookCellOutputItem.text('#this is a test of markdown', 'text/markdown'));
+			outputItems.push(vscode.NotebookCellOutputItem.text('Summary of code: ' + summarydata.explanation, 'text/markdown'));
 			// push a new CellOutputItem with the json parsed text of the cell
-			outputItems.push(vscode.NotebookCellOutputItem.text('print(\'this is the code.\')', 'text/html'));
+			outputItems.push(vscode.NotebookCellOutputItem.text(generatedCode.code, 'text/plain'));
 
 			// create a new NotebookCellOutput with the outputItems array
 			const output = new vscode.NotebookCellOutput(outputItems);
