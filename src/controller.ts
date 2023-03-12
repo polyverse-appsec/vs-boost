5dea7c1c5dd28c5de8892afe9976b53d916769d3
Alexander Gounares <alex@gounares.com>
2023-03-11 23:31:05 -0800

    get the output looking better, refactor execution code

--- src/controller.ts
+++ src/controller.ts
@@ -26,11 +26,35 @@ export class BoostKernel {
 
 	private _executeAll(cells: vscode.NotebookCell[], _notebook: vscode.NotebookDocument, _controller: vscode.NotebookController): void {
 		for (const cell of cells) {
+			//if the cell is generated code, don't run it by default, the original code cell will run it
+			if (cell.metadata.type === 'generatedCode') {
+				return;
+			}
 			this._doExecution(cell);
 		}
 	}
 
 	private async _doExecution(cell: vscode.NotebookCell): Promise<void> {
+
+		// we basically run two executions, one for the original code to generate a summary
+		// and one for the generated code
+
+		// if the cell is original code, run the summary generation
+		if (cell.metadata.type === 'originalCode') {
+			this._doSummaryExecution(cell)
+				.then((summaryCell) => {
+					if( summaryCell	){
+						this._doGeneratedCodeExecution(summaryCell);
+					}
+				});
+		} else if (cell.metadata.type === 'generatedCode') {
+			// if the cell is generated code, run the generated code
+			this._doGeneratedCodeExecution(cell);
+		}
+	}
+
+	private async _doSummaryExecution(cell: vscode.NotebookCell): Promise<vscode.NotebookCell | undefined>{
+
 		const execution = this._controller.createNotebookCellExecution(cell);
 
 		execution.executionOrder = ++this._executionOrder;
@@ -51,44 +75,23 @@ export class BoostKernel {
 			const summarydata = response.data;
 			// print the response to the console
 			console.log(summarydata);
-
-			// now take the summary and using axios send it to localhost:8080/generate/python with the summary in a json object summary=summary
-			const response2 = await axios.post('http://localhost:8080/generate/python', { explanation: summarydata.explanation });
-
-			const generatedCode = await response2.data;
-
-			// print the response to the console
-			console.log(generatedCode);
-
-
-
+/*
 			const outputItems: vscode.NotebookCellOutputItem[] = [];
 			// push a new CellOutputItem with the json parsed text of the cell with the markdown string of "this is a test"
 			outputItems.push(vscode.NotebookCellOutputItem.text('Summary of code: ' + summarydata.explanation, 'text/markdown'));
-			// push a new CellOutputItem with the json parsed text of the cell
-			// TODO: turn this to the language of the generated code
-			/* VS Code will render these mimetypes as code in a built-in editor:
-
-			text/x-json
-			text/x-javascript
-			text/x-html
-			text/x-rust
-			... text/x-LANGUAGE_ID for any other built-in or installed languages.*/
-
-			outputItems.push(vscode.NotebookCellOutputItem.text(generatedCode.code, 'text/x-python'));
 
 			// create a new NotebookCellOutput with the outputItems array
 			const output = new vscode.NotebookCellOutput(outputItems);
 
 			execution.replaceOutput(output);
-
+*/
 			execution.end(true, Date.now());
 
 			console.log('done, trying to add cell');
 			// now try to add a new cell to the notebook with the generated summary
-			const newCell = new vscode.NotebookCellData(vscode.NotebookCellKind.Markup, summarydata.explanation, 'markdown');
+			const newCell = new vscode.NotebookCellData(vscode.NotebookCellKind.Code, summarydata.explanation, 'markdown');
 			const cells = [newCell];
-			newCell.metadata = {"id": currentId};
+			newCell.metadata = {"id": currentId, "type": "generatedCode"};
 
 			const currentNotebook = vscode.window.activeNotebookEditor?.notebook;
 			if (currentNotebook) {
@@ -111,8 +114,74 @@ export class BoostKernel {
 					]);
 				}
 				await vscode.workspace.applyEdit(edit);
+				return currentNotebook.getCells()[cell.index + 1];
 				console.log('added cell')
 			}
+			return undefined;
+		} catch (err) {
+			execution.replaceOutput([new vscode.NotebookCellOutput([
+				vscode.NotebookCellOutputItem.error(err as Error)
+			])]);
+			execution.end(false, Date.now());
+		}
+		return undefined;
+	}
+
+	private async _doGeneratedCodeExecution(cell: vscode.NotebookCell): Promise<void> {
+
+		//if cell is undefined or metadata is undefined, return
+		if (!cell || !cell.metadata) {
+			return;
+		}
+		const currentId = cell.metadata.id;
+		const execution = this._controller.createNotebookCellExecution(cell);
+
+		execution.executionOrder = ++this._executionOrder;
+		execution.start(Date.now());
+
+		try {
+			//make an array of CellOputputItems with the type NotebookCellOutputItem
+
+			// get the code from the cell
+			const summarydata = cell.document.getText();
+
+			// using axios, make a web POST call to localhost:8080/explain with the code as in a json object code=code
+			//const response = await axios.post('http://localhost:8080/explain', { code: code });
+
+
+			//const summarydata = response.data;
+			// print the response to the console
+			console.log(summarydata);
+
+			// now take the summary and using axios send it to localhost:8080/generate/python with the summary in a json object summary=summary
+			const response2 = await axios.post('http://localhost:8080/generate/python', { explanation: summarydata });
+
+			const generatedCode = await response2.data;
+
+			// print the response to the console
+			console.log(generatedCode);
+
+
+
+			const outputItems: vscode.NotebookCellOutputItem[] = [];
+		
+			// TODO: turn this to the language of the generated code
+			/* VS Code will render these mimetypes as code in a built-in editor:
+
+			text/x-json
+			text/x-javascript
+			text/x-html
+			text/x-rust
+			... text/x-LANGUAGE_ID for any other built-in or installed languages.*/
+
+			outputItems.push(vscode.NotebookCellOutputItem.text(generatedCode.code, 'text/x-python'));
+
+			// create a new NotebookCellOutput with the outputItems array
+			const output = new vscode.NotebookCellOutput(outputItems);
+
+			execution.replaceOutput(output);
+
+			execution.end(true, Date.now());
 
 		} catch (err) {
 			execution.replaceOutput([new vscode.NotebookCellOutput([
@@ -120,5 +189,5 @@ export class BoostKernel {
 			])]);
 			execution.end(false, Date.now());
 		}
-	}
+	}	
 }
