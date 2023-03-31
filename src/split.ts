7cc23a524203fecce8debc076375798316fabbb8
Alexander Gounares <alex@gounares.com>
2023-03-30 19:04:59 -0700

    fix nesting counting bug

--- src/split.ts
+++ src/split.ts
@@ -1,44 +1,51 @@
 function splitCode(code: string): string[] {
-    const chunks: string[] = [];
-    const lines = code.split('\n');
-    let currentChunk = '';
-    let chunkCount = 0;
-    let nestingCount = 0;
-    let inNest = false;
-  
-    for (const line of lines) {
-      currentChunk += line + '\n';
-  
-      if (currentChunk.split(' ').length > 2000 || currentChunk.length > 4000) {
-        chunks.push(currentChunk);
-        currentChunk = '';
-        chunkCount++;
-      }
-  
-      if (line.includes('{')) {
-        nestingCount++;
-        inNest = true;
-      }
-  
-      if (line.includes('}')) {
-        nestingCount--;
-      }
-  
-      if (nestingCount === 0 && currentChunk !== '' && inNest) {
-        chunks.push(currentChunk);
-        currentChunk = '';
-        chunkCount++;
-        inNest = false;
-      }
-    }
-  
-    if (currentChunk.trim() !== '' && currentChunk !== '') {
+  const chunks: string[] = [];
+  const lines = code.split('\n');
+  let lineno = 0;
+  let currentChunk = '';
+  let chunkCount = 0;
+  let nestingCount = 0;
+  let inNest = false;
+
+  for (const line of lines) {
+    currentChunk += line + '\n';
+    lineno++;
+
+    if (currentChunk.split(' ').length > 2000 || currentChunk.length > 4000) {
       chunks.push(currentChunk);
+      currentChunk = '';
       chunkCount++;
     }
-  
-    return chunks;
+
+    const leftBraces = (line.match(/{/g) || []).length;
+    const rightBraces = (line.match(/}/g) || []).length;
+
+    if (leftBraces > 0) {
+      nestingCount += leftBraces;
+
+      inNest = true;
+    }
+
+    if (rightBraces > 0) {
+      nestingCount -= rightBraces;
+    }
+
+    if (nestingCount === 0 && currentChunk !== '' && inNest) {
+      chunks.push(currentChunk);
+      currentChunk = '';
+      chunkCount++;
+      inNest = false;
+    }
   }
+
+  if (currentChunk.trim() !== '' && currentChunk !== '') {
+    chunks.push(currentChunk);
+    chunkCount++;
+  }
+
+  return chunks;
+}
+
   
   function getFileExtension(filename: string): string {
     const lastIndex = filename.lastIndexOf(".");
