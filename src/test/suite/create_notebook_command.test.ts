45b91aa59a75752a4b0107a2d67c058761ca4c7e
Stephen Fisher <stephen@polyverse.com>
2023-04-02 23:37:37 -0700

    minor tweak of hotExit setting

--- src/test/suite/create_notebook_command.test.ts
+++ src/test/suite/create_notebook_command.test.ts
@@ -1,7 +1,5 @@
 import * as vscode from 'vscode';
 import * as assert from 'assert';
-import { debug } from 'console';
-import * as crypto from 'crypto';
 
 suite('Notebook Command', function() {
 
@@ -12,6 +10,10 @@ suite('Notebook Command', function() {
   test('Create Notebook Command Test', async function() {
 
     this.timeout(5000);
+
+    // we need to avoid hanging on the save dialog when exiting visual studio
+    //    so we disable save on exit/shutdown
+    await vscode.workspace.getConfiguration().update('files.hotExit', 'off', vscode.ConfigurationTarget.Global);
 
     // Execute the "createJsonNotebook" command
     await vscode.commands.executeCommand('polyverse-boost-notebook.createJsonNotebook',
@@ -26,8 +28,5 @@ suite('Notebook Command', function() {
 
     assert.notEqual(notebooks.length, 0, 'Notebook not created!');
 
-    // we need to avoid hanging on the save dialg when exiting visual studio
-    //    so we disable save on exit/shutdown
-    await vscode.workspace.getConfiguration().update('files.hotExit', 'off', vscode.ConfigurationTarget.Global);
   });
 });
