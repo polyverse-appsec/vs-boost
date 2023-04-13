d55588706f28d377269a1977200c83394b66a85c
Stephen Fisher <stephen@polyverse.com>
2023-04-13 01:38:28 -0700

    Add better error message for HTTP 400 This is potentially bad input, and we may need to know about it at support Or it may be temporary OpenAI issue

--- src/base_controller.ts
+++ src/base_controller.ts
@@ -194,6 +194,11 @@ export class KernelControllerBase {
         } catch (err : any) {
             if (err.response) {
                 switch (err.response.status) {
+                    case 400: // bad request - potential bad input from Boost extension or invalid source
+                        throw new Error(
+                            "Unable to process this source code. This can be caused by a temporary issue with the " +
+                            "Boost Cloud Service, or by an issue in the source input. Please try again, and if the " +
+                            "problem persists, please contact Boost Support.");
                     case 401: // authorization error - likely GitHub issue
                         throw new Error(
                             "Unable to use your GitHub authorized account to access the Boost Cloud Service. " +
