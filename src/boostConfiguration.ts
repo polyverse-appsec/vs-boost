a10bf8ea7727767ec64e41dc318afdaa4bc6b461
Alexander Gounares <alex@gounares.com>
2023-04-29 14:45:08 -0700

    get version information

--- src/boostConfiguration.ts
+++ src/boostConfiguration.ts
@@ -1,5 +1,8 @@
 import { NOTEBOOK_TYPE } from "./extension";
 import { workspace } from "vscode";
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export class BoostConfiguration {
   
@@ -72,4 +75,38 @@ class Defaults {
     public static readonly processFoldersInASingleNotebookName = "processFoldersInASingleNotebook";
     public static readonly processFoldersInASingleNotebookValue = true;
 }
-  
+
+//keep a global variable for the extension version, start as empty string
+let cachedVersion = "";
+
+export function getCurrentExtensionVersion(): string | undefined {
+    const extensionId = 'polyversecorporation.polyverse-boost-notebook'; // Replace this with your extension's ID
+    const extension = vscode.extensions.getExtension(extensionId);
+
+    if (cachedVersion !== "") {
+        return cachedVersion;
+    }
+
+    if (!extension) {
+        vscode.window.showErrorMessage('Extension not found.');
+        return undefined;
+    }
+
+    const packageJsonPath = path.join(extension.extensionPath, 'package.json');
+    
+    try {
+        const packageJsonData = fs.readFileSync(packageJsonPath, 'utf-8');
+        const packageJson = JSON.parse(packageJsonData);
+        const version = packageJson.version;
+
+        if (!version) {
+            vscode.window.showErrorMessage('Extension version not found.');
+            return undefined;
+        }
+        cachedVersion = version;
+        return version;
+    } catch (error) {
+        vscode.window.showErrorMessage('Error reading package.json: ' + (error as Error).message);
+        return undefined;
+    }
+}
--- src/portal.ts
+++ src/portal.ts
@@ -1,21 +1,41 @@
 import * as vscode from 'vscode';
 import axios from 'axios';
+import { BoostConfiguration } from './boostConfiguration';
+import { fetchGithubSession } from './base_controller';
+import { getCurrentExtensionVersion } from './boostConfiguration';
 
-import { fetchGithubSession } from './base_controller'; 
 
+function serviceEndpoint(): string {
+    switch (BoostConfiguration.cloudServiceStage)
+    {
+        case "local":
+            return 'http://127.0.0.1:8000/customer_portal';
+        case 'dev':
+            return 'http://127.0.0.1:8000/customer_portal';
+        case "test":
+            return 'http://127.0.0.1:8000/customer_portal';
+        case 'staging':
+        case 'prod':
+        default:
+            return 'http://127.0.0.1:8000/customer_portal';
+    }
+}
 
 export function registerCustomerPortalCommand(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.commands.registerCommand('polyverse-boost-notebook.customerPortal', async () => {
             let session = await fetchGithubSession();       // get the session
+            let version = getCurrentExtensionVersion();     // get the extension version
             let payload = {
                 "session": session.accessToken,
                 "organization": "polyverse",
-                "version": "1.0.0"
+                "version": version
             };
 
+            let endpoint = serviceEndpoint();
+
             const response = await axios.post(
-                "http://localhost:8000/customer_portal",
+                endpoint,
                 payload);
             
             let url = response.data['portal_url'];
