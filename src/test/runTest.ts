33691138187b2b0ea5a19c696bb7a2d961fa3a44
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-05-13 22:10:38 -0500

    Cleanup target file in test suite on failure

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -8,28 +8,32 @@ async function main(argv : string[]) {
 	try {
         const filename = argv[2]?.split(",");
         let targetTestInputPath : string | undefined = undefined;
-        if (filename) {
-            console.log('Target Test filename:', filename);
-            const targetTestInput = { filename };
-            targetTestInputPath = path.resolve(__dirname, '../test/resources', 'targetTestInput.json');
-            fs.writeFileSync(targetTestInputPath, JSON.stringify(targetTestInput, null, 2));
-            console.log(`Created ${targetTestInputPath}`);
-        }
+        try
+        {
+            if (filename) {
+                console.log('Target Test filename:', filename);
+                const targetTestInput = { filename };
+                targetTestInputPath = path.resolve(__dirname, '../test/resources', 'targetTestInput.json');
+                fs.writeFileSync(targetTestInputPath, JSON.stringify(targetTestInput, null, 2));
+                console.log(`Created ${targetTestInputPath}`);
+            }
 
-        // The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+            // The folder containing the Extension Manifest package.json
+            // Passed to `--extensionDevelopmentPath`
+            const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+            // The path to test runner
+            // Passed to --extensionTestsPath
+            const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: filename });
+            // Download VS Code, unzip it and run the integration test
+            await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: filename });
 
-        if (targetTestInputPath) {
-            fs.unlinkSync(targetTestInputPath);
-            console.log(`Deleted ${targetTestInputPath}`);
+        } finally {
+            if (targetTestInputPath) {
+                fs.unlinkSync(targetTestInputPath);
+                console.log(`Deleted ${targetTestInputPath}`);
+            }
         }
 	} catch (err) {
 		console.error('Failed to run tests', err);
