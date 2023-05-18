658e871c70cde1b047d7c0f7e499faab8f855589
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-05-18 10:07:59 -0500

    Remove direct VSCode reference Minor style cleanup on long strings

--- src/convert_html.ts
+++ src/convert_html.ts
@@ -1,22 +1,29 @@
-import { BoostNotebook } from './jupyter_notebook';
-import * as vscode from 'vscode';
+import { BoostNotebook, NotebookCellKind } from './jupyter_notebook';
 import {marked} from 'marked';
 import hljs from 'highlight.js';
+
+const cellStyleSheet = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css';
+const mermaidScript = 'https://cdnjs.cloudflare.com/ajax/libs/mermaid/8.14.1/mermaid.min.js';
 
 export async function convertNotebookToHTML(notebook: BoostNotebook) {
 
     const cells = notebook.cells;
 
     // convert cells to html
-    let html = '<html><head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css"> <script type="module">import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs"; mermaid.initialize({ startOnLoad: true });</script>  <style>@page {margin: 2cm;}</style></head><body>';
+    let html = `<html><head><link rel="stylesheet" href="${cellStyleSheet}"> ` +
+               `<script type="module">import mermaid from "${mermaidScript}";` +
+               ` mermaid.initialize({ startOnLoad: true });</script>  <style>@page {margin: 2cm;}</style></head><body>`;
+
     for (let cell of cells) {
-        if (cell.kind === vscode.NotebookCellKind.Markup) {
+        // we use Jupyter type to avoid direct dependencies on VS Code
+        // however tbis should be identical value in JSON
+        if (cell.kind === NotebookCellKind.Markup) {
             html += marked.parse(cell.value, {
                 highlight: function (code, lang) {
                     return hljs.highlightAuto(code, [lang]).value;
                 }
             });
-        } else if (cell.kind === vscode.NotebookCellKind.Code) {
+        } else if (cell.kind === NotebookCellKind.Code) {
           const value = hljs.highlightAuto(cell.value);
             html += '<pre><code>' + value.value + '</code></pre>';
         }
