4ebc0f74d9d3e712b87f37bd0268e474682fcb0c
Alexander Gounares <alex@gounares.com>
2023-05-27 20:55:15 -0700

    fix watch mode

--- src/dashboard/main.ts
+++ src/dashboard/main.ts
@@ -4,6 +4,7 @@ import {
 } from "@vscode/webview-ui-toolkit";
 
 provideVSCodeDesignSystem().register(vsCodeButton());
-
+console.log('hello world XXYYZZ');
+console.log("hello world XXYYZZ again");
 //to get a recompile
 //another comment
