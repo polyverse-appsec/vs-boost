f1b881ea771290f12dbe4951fb8197e307aa7408
Alexander Gounares <alex@gounares.com>
2023-05-30 23:27:03 -0700

    first pass summary view tidy up

--- src/dashboard/summary/main.ts
+++ src/dashboard/summary/main.ts
@@ -5,11 +5,32 @@ import {
   vsCodeDataGrid,
   vsCodeDataGridCell,
   vsCodeDataGridRow,
-  vsCodeCheckbox
+  vsCodeCheckbox,
+  Button
 } from "@vscode/webview-ui-toolkit";
 
 provideVSCodeDesignSystem().register(vsCodeButton(), vsCodeBadge(), vsCodeDataGrid(), vsCodeDataGridCell(), vsCodeDataGridRow(), vsCodeCheckbox());
-console.log('hello world XXYYZZ');
-console.log("hello world XXYYZZ again");
-//to get a recompile
-//another comment
+
+const vscode = acquireVsCodeApi();
+
+// Just like a regular webpage we need to wait for the webview
+// DOM to load before we can reference any of the HTML elements
+// or toolkit components
+window.addEventListener("load", main);
+
+// Main function that gets executed once the webview DOM loads
+function main() {
+  // To get improved type annotations/IntelliSense the associated class for
+  // a given toolkit component can be imported and used to type cast a reference
+  // to the element (i.e. the `as Button` syntax)
+  const howdyButton = document.getElementById("update_analysis") as Button;
+  howdyButton?.addEventListener("click", handleAnalyzeAllClick);
+}
+
+// Callback function that is executed when the howdy button is clicked
+function handleAnalyzeAllClick() {
+  //TODO: we need to show what is checked in the grid.
+  vscode.postMessage({
+    command: "update_analysis"
+  });
+}
