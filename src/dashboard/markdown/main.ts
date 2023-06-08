1d0c337e249d114138cd15189463449c6c398e30
Alexander Gounares <alex@gounares.com>
2023-06-07 21:37:12 -0700

    more scaffolding for markdown viewer

---
+++ src/dashboard/markdown/main.ts
@@ -0,0 +1,18 @@
+import {
+  provideVSCodeDesignSystem,
+  vsCodeButton
+} from "@vscode/webview-ui-toolkit";
+
+provideVSCodeDesignSystem().register(vsCodeButton());
+
+const vscode = acquireVsCodeApi();
+
+// Just like a regular webpage we need to wait for the webview
+// DOM to load before we can reference any of the HTML elements
+// or toolkit components
+window.addEventListener("load", main);
+
+// Main function that gets executed once the webview DOM loads
+function main() {
+
+}
