cff4251752b2531219c27c37f7f244fe4247f34b
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-06-09 02:12:20 -0700

    Fix "summarize" kernel error

--- src/start_view.ts
+++ src/start_view.ts
@@ -85,7 +85,7 @@ export class BoostStartViewProvider implements vscode.WebviewViewProvider {
                         await vscode.commands.executeCommand(NOTEBOOK_TYPE + '.' + BoostCommands.refreshProjectData);
 
                         // summary across all files
-                        await vscode.commands.executeCommand(NOTEBOOK_TYPE + '.' + BoostCommands.processCurrentFolder, undefined, summarizeKernelName);
+                        await vscode.commands.executeCommand(NOTEBOOK_TYPE + '.' + BoostCommands.processCurrentFolder, undefined, getKernelName(summarizeKernelName));
 					}
 				case 'open_file':
 					{
