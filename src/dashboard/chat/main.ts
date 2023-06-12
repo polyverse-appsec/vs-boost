32d9018b0299038f6cd8fb3885166661e215420a
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-06-12 02:07:10 -0700

    Fix paste in chat window

--- src/dashboard/chat/main.ts
+++ src/dashboard/chat/main.ts
@@ -1,18 +1,18 @@
 import {
-  provideVSCodeDesignSystem,
-  vsCodeButton,
-  vsCodeTextArea,
-  vsCodeRadio,
-  vsCodeRadioGroup,
-  vsCodePanels,
-  vsCodePanelTab,
-  vsCodePanelView,
-  vsCodeProgressRing,
-  Button
+    provideVSCodeDesignSystem,
+    vsCodeButton,
+    vsCodeTextArea,
+    vsCodeRadio,
+    vsCodeRadioGroup,
+    vsCodePanels,
+    vsCodePanelTab,
+    vsCodePanelView,
+    vsCodeProgressRing,
+    Button
 } from "@vscode/webview-ui-toolkit";
 
 provideVSCodeDesignSystem().register(vsCodeButton(), vsCodeTextArea(), vsCodeRadio(), vsCodeRadioGroup(),
-  vsCodePanels(), vsCodePanelTab(), vsCodePanelView(), vsCodeProgressRing());
+    vsCodePanels(), vsCodePanelTab(), vsCodePanelView(), vsCodeProgressRing());
 
 const vscode = acquireVsCodeApi();
 
@@ -23,66 +23,82 @@ window.addEventListener("load", main);
 
 // Main function that gets executed once the webview DOM loads
 function main() {
-  // To get improved type annotations/IntelliSense the associated class for
-  // a given toolkit component can be imported and used to type cast a reference
-  // to the element (i.e. the `as Button` syntax)
-  const howdyButton = document.getElementById("send") as Button;
-  howdyButton?.addEventListener("click", handleSendClick);
+    // To get improved type annotations/IntelliSense the associated class for
+    // a given toolkit component can be imported and used to type cast a reference
+    // to the element (i.e. the `as Button` syntax)
+    const howdyButton = document.getElementById("send") as Button;
+    howdyButton?.addEventListener("click", handleSendClick);
 
-  //now add listeners for the add and close buttons. we don't know how many so we need to loop
-  const closeButtons = document.getElementsByClassName("tab-close-button") as HTMLCollectionOf<Button>;
-  for (let i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener("click", handleCloseClick);
-  }
+    // Add event listener for the "paste" event on the text area
+    const promptTextArea = document.getElementById("prompt") as HTMLTextAreaElement;
+    promptTextArea?.addEventListener("paste", handlePaste);
 
-  //add a listener for the add button
-  const addButton = document.getElementById("tab-add-button") as Button;
-  addButton?.addEventListener("click", handleAddClick);
+    //now add listeners for the add and close buttons. we don't know how many so we need to loop
+    const closeButtons = document.getElementsByClassName("tab-close-button") as HTMLCollectionOf<Button>;
+    for (let i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].addEventListener("click", handleCloseClick);
+    }
+
+    //add a listener for the add button
+    const addButton = document.getElementById("tab-add-button") as Button;
+    addButton?.addEventListener("click", handleAddClick);
 
 }
 
 function handleAddClick() {
-  console.log("add click");
-  vscode.postMessage({
-    command: "add-chat"
-  });
+    console.log("add click");
+    vscode.postMessage({
+        command: "add-chat"
+    });
 }
 
 //for the close click, we need to know which one was clicked. we can get that from the id
 function handleCloseClick(event: Event) {
-  const id = (event.target as HTMLElement).id;
-  vscode.postMessage({
-    command: "close-chat",
-    chatindex: id.split("-")[1]
-  });
+    const id = (event.target as HTMLElement).id;
+    vscode.postMessage({
+        command: "close-chat",
+        chatindex: id.split("-")[1]
+    });
 }
 
+function handlePaste(event: ClipboardEvent) {
+    const clipboardData = event.clipboardData || (window as any).clipboardData;
+    const pastedContent = clipboardData.getData("text");
+
+    // Handle the pasted content as needed
+    // Example: Update the value of the text area with the pasted content
+    const promptTextArea = event.target as HTMLTextAreaElement;
+    promptTextArea.value += pastedContent;
+
+    // Prevent the default paste behavior
+    event.preventDefault();
+}
 
 // Callback function that is executed when the howdy button is clicked
 function handleSendClick() {
-  // get the value of the radio button
-  const gpt35 = document.getElementById("gpt35") as HTMLElement;
-  const checked = gpt35.getAttribute("current-checked");
-  const chatGroup = document.getElementById("chat-group") as HTMLElement;
-  const chatid = chatGroup.getAttribute("activeid");
-  const chatindex = chatid?.split("-")[1];
+    // get the value of the radio button
+    const gpt35 = document.getElementById("gpt35") as HTMLElement;
+    const checked = gpt35.getAttribute("current-checked");
+    const chatGroup = document.getElementById("chat-group") as HTMLElement;
+    const chatid = chatGroup.getAttribute("activeid");
+    const chatindex = chatid?.split("-")[1];
 
-  let model = "gpt-4";
-  if( checked === "true" ) {
-    model = "gpt-3.5-turbo";
-  } 
+    let model = "gpt-4";
+    if (checked === "true") {
+        model = "gpt-3.5-turbo";
+    }
 
-  const sendButton = document.getElementById("send") as Button;
-  const progressRing = document.getElementById("progress") as HTMLElement;
-  //disable the button and show the progress ring by adding/removing the hidden attribute
-  sendButton.setAttribute("hidden", "");
-  progressRing.removeAttribute("hidden");
+    const sendButton = document.getElementById("send") as Button;
+    const progressRing = document.getElementById("progress") as HTMLElement;
+    //disable the button and show the progress ring by adding/removing the hidden attribute
+    sendButton.setAttribute("hidden", "");
+    progressRing.removeAttribute("hidden");
 
-  
-  vscode.postMessage({
-    chatindex: chatindex,
-    model: model,
-    command: "newprompt",
-    prompt: (document.getElementById("prompt") as HTMLTextAreaElement)?.value 
-  });
+
+    vscode.postMessage({
+        chatindex: chatindex,
+        model: model,
+        command: "newprompt",
+        prompt: (document.getElementById("prompt") as HTMLTextAreaElement)?.value
+    });
 }
