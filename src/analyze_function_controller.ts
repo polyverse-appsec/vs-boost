a5082f5c0a84c119fd16a4884cfc998ed20ab815
Alexander Gounares <alex@gounares.com>
2023-06-22 01:43:52 -0700

    get output format and meta for analysis function

--- src/analyze_function_controller.ts
+++ src/analyze_function_controller.ts
@@ -44,10 +44,20 @@ export class BoostAnalyzeFunctionKernel extends KernelControllerBase {
 	}
 
     onKernelOutputItem(response: any): string {
-        if (response.analysis === undefined) {
+        if (response.details === undefined) {
             throw new Error("Unexpected missing data from Boost Service");
         }
-        return response.analysis;
+        //loop through the details field and create a markdown string to return 
+        let markdown = "# Boost Bug Analysis\n\n";
+
+        response.details.forEach((bug: any, index: number) => {
+            markdown += `${index + 1}. **Severity**: ${bug.severity}/10\n`;
+            markdown += `   **Line Number**: ${bug.lineNumber}\n`;
+            markdown += `   **Bug Type**: ${bug.bugType}\n`;
+            markdown += `   **Description**: ${bug.description}\n`;
+            markdown += `   **Solution**: ${bug.solution}\n\n`;
+        });
+        return markdown;
     }
 
     localizeError(error: Error): Error {
