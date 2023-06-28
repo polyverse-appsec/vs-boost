93bbd55f9498cd767f300d8a11ac7ee6696b1421
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-06-28 12:24:07 -0700

    Add guidelines to Start view Gracefully log/handle errors on view methods

--- src/start_view.ts
+++ src/start_view.ts
@@ -2,87 +2,107 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as _ from 'lodash';
 import { BoostExtension } from './BoostExtension';
-import { getOrCreateBlueprintUri, BoostCommands, getKernelName} from './extension';
+import { getOrCreateBlueprintUri, BoostCommands, getKernelName, getBoostFile, BoostFileType } from './extension';
 import { boostLogging } from './boostLogging';
 
 
 
 export class BoostStartViewProvider implements vscode.WebviewViewProvider {
 
-	public static readonly viewType = 'polyverse-boost-start-view';
+    public static readonly viewType = 'polyverse-boost-start-view';
 
-	private _view?: vscode.WebviewView;
+    private _view?: vscode.WebviewView;
 
-	constructor(
-		private readonly context: vscode.ExtensionContext,
-		private _boostExtension: BoostExtension
-	) { }
+    constructor(
+        private readonly context: vscode.ExtensionContext,
+        private _boostExtension: BoostExtension
+    ) { }
 
-	public resolveWebviewView(
-		webviewView: vscode.WebviewView,
-		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken,
-	) {
-		this._view = webviewView;
+    public resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        _: vscode.WebviewViewResolveContext,
+        _token: vscode.CancellationToken,
+    ) {
+        try {
+            this._resolveWebviewView(webviewView, _, _token);
+        } catch (e) {
+            boostLogging.error(`Could not load Boost Start View due to ${e}`, false);
+        }
+    }
 
-		const boostdata = this._boostExtension.getBoostProjectData();
+    _resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        _: vscode.WebviewViewResolveContext,
+        _token: vscode.CancellationToken,
+    ) {
+        this._view = webviewView;
 
-		webviewView.webview.options = {
-			// Allow scripts in the webview
-			enableScripts: true,
+        const boostdata = this._boostExtension.getBoostProjectData();
 
-			localResourceRoots: [
-				this.context.extensionUri
-			]
-		};
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
 
-		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview, boostdata);
+            localResourceRoots: [
+                this.context.extensionUri
+            ]
+        };
 
-		webviewView.webview.onDidReceiveMessage(async data => {
-			switch (data.command) {
-				case 'open_file':
-					{
-						const path = data.file;
-						const blueprintUri = await getOrCreateBlueprintUri(this.context, path);
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview, boostdata);
+
+        webviewView.webview.onDidReceiveMessage(async data => {
+            switch (data.command) {
+                case 'open_file':
+                    {
+                        const path = data.file;
+                        const blueprintUri = await getOrCreateBlueprintUri(this.context, path);
                         try {
                             const document = await vscode.workspace.openNotebookDocument(blueprintUri);
                             await vscode.window.showNotebookDocument(document);
                         } catch (e) {
                             boostLogging.error(`Could not open Boost Project Summary ${blueprintUri} due to ${e}`, true);
                         }
-					}
-					break;
+                    }
+                    break;
 
-				case 'show_summary':
-					{
-						vscode.commands.executeCommand('polyverse-boost-summary-view.focus');
-					}
-					break;
-			}
-		});
-	}
+                case 'show_summary':
+                    {
+                        vscode.commands.executeCommand('polyverse-boost-summary-view.focus');
+                    }
+                    break;
+            }
+        });
+    }
 
-	public refresh() {
-		if (this._view) {
-			this._view.webview.html = this._getHtmlForWebview(this._view.webview, this._boostExtension.getBoostProjectData());
-			this._view.show?.(true);
-		}
-	}
+    public refresh() {
+        try {
+            this._refresh();
+        } catch (e) {
+            boostLogging.error(`Could not refresh Boost Start View due to ${e}`, false);
+        }
+    }
+
+    _refresh() {
+        if (this._view) {
+            this._view.webview.html = this._getHtmlForWebview(this._view.webview, this._boostExtension.getBoostProjectData());
+            this._view.show?.(true);
+        }
+    }
 
     private _getHtmlForWebview(webview: vscode.Webview, boostdata: any) {
 
         const htmlPathOnDisk = vscode.Uri.joinPath(this.context.extensionUri, 'resources', 'dashboard', 'start.html');
-		const jsPathOnDisk = vscode.Uri.joinPath(this.context.extensionUri, 'out', 'dashboard', 'start', 'main.js');
+        const jsPathOnDisk = vscode.Uri.joinPath(this.context.extensionUri, 'out', 'dashboard', 'start', 'main.js');
         const jsSrc = webview.asWebviewUri(jsPathOnDisk);
-		const nonce = 'nonce-123456'; // TODO: add a real nonce here
+        const nonce = 'nonce-123456'; // TODO: add a real nonce here
         const rawHtmlContent = fs.readFileSync(htmlPathOnDisk.fsPath, 'utf8');
-		
-		const blueprintFile = boostdata.summary.summaryUrl; 
 
+        const blueprintFile = boostdata.summary.summaryUrl;
+        const guidelinesFile = getBoostFile(blueprintFile, BoostFileType.guidelines, false).fsPath;
 
         const template = _.template(rawHtmlContent);
-        const htmlContent = template({ jsSrc, nonce, boostdata, blueprintFile});
-    
+        const htmlContent = template({ jsSrc, nonce, boostdata, blueprintFile, guidelinesFile });
+
         return htmlContent;
     }
 }
