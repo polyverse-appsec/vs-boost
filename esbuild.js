49d9a43c216f833c0ed2289c363a693bba9e0987
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-07-11 17:59:22 +0200

    Minor updates to logging and error handling in esbuild script

--- esbuild.js
+++ esbuild.js
@@ -6,123 +6,135 @@ const { build } = require("esbuild");
 
 /** @type BuildOptions */
 const baseConfig = {
-  bundle: true,
-  minify: process.env.NODE_ENV === "production",
-  sourcemap: process.env.NODE_ENV !== "production",
+    bundle: true,
+    minify: process.env.NODE_ENV === "production",
+    sourcemap: process.env.NODE_ENV !== "production",
 };
 
 // Config for extension source code (to be run in a Node-based context)
 /** @type BuildOptions */
 const extensionConfig = {
-  ...baseConfig,
-  platform: "node",
-  mainFields: ["module", "main"],
-  format: "cjs",
-  entryPoints: ["./src/extension.ts"],
-  outfile: "./out/extension.js",
-  external: ["vscode"],
+    ...baseConfig,
+    platform: "node",
+    mainFields: ["module", "main"],
+    format: "cjs",
+    entryPoints: ["./src/extension.ts"],
+    outfile: "./out/extension.js",
+    external: ["vscode"],
 };
 
 // Config for webview source code (to be run in a web-based context)
 /** @type BuildOptions */
 const summaryConfig = {
-  ...baseConfig,
-  target: "es2020",
-  format: "esm",
-  entryPoints: ["./src/dashboard/summary/main.ts"],
-  outfile: "./out/dashboard/summary/main.js",
+    ...baseConfig,
+    target: "es2020",
+    format: "esm",
+    entryPoints: ["./src/dashboard/summary/main.ts"],
+    outfile: "./out/dashboard/summary/main.js",
 };
 
 // Config for webview source code (to be run in a web-based context)
 /** @type BuildOptions */
 const chatConfig = {
-  ...baseConfig,
-  target: "es2020",
-  format: "esm",
-  entryPoints: ["./src/dashboard/chat/main.ts"],
-  outfile: "./out/dashboard/chat/main.js",
+    ...baseConfig,
+    target: "es2020",
+    format: "esm",
+    entryPoints: ["./src/dashboard/chat/main.ts"],
+    outfile: "./out/dashboard/chat/main.js",
 };
 
 // Config for webview source code (to be run in a web-based context)
 /** @type BuildOptions */
 const startConfig = {
-  ...baseConfig,
-  target: "es2020",
-  format: "esm",
-  entryPoints: ["./src/dashboard/start/main.ts"],
-  outfile: "./out/dashboard/start/main.js",
+    ...baseConfig,
+    target: "es2020",
+    format: "esm",
+    entryPoints: ["./src/dashboard/start/main.ts"],
+    outfile: "./out/dashboard/start/main.js",
 };
 
 // Config for webview source code (to be run in a web-based context)
 /** @type BuildOptions */
 const markdownConfig = {
-  ...baseConfig,
-  target: "es2020",
-  format: "esm",
-  entryPoints: ["./src/dashboard/markdown/main.ts"],
-  outfile: "./out/dashboard/markdown/main.js",
+    ...baseConfig,
+    target: "es2020",
+    format: "esm",
+    entryPoints: ["./src/dashboard/markdown/main.ts"],
+    outfile: "./out/dashboard/markdown/main.js",
 };
 
 // This watch config adheres to the conventions of the esbuild-problem-matchers
 // extension (https://github.com/connor4312/esbuild-problem-matchers#esbuild-via-js)
 /** @type BuildOptions */
 const watchConfig = {
-  watch: {
-    onRebuild(error, result) {
-      console.log("[watch] build started");
-      if (error) {
-        error.errors.forEach((error) =>
-          console.error(
-            `> ${error.location.file}:${error.location.line}:${error.location.column}: error: ${error.text}`
-          )
-        );
-      } else {
-        console.log("[watch] build finished");
-      }
+    watch: {
+        onRebuild(error, result) {
+            console.log("[WATCH] build started");
+            if (error) {
+                error.errors.forEach((error) =>
+                    console.error(
+                        `> ${error.location.file}:${error.location.line}:${error.location.column}: ERROR MSG: ${error.text}`
+                    )
+                );
+            } else {
+                console.log("[WATCH] build finished");
+            }
+        },
     },
-  },
 };
 
 // Build script
 (async () => {
-  const args = process.argv.slice(2);
-  try {
-    if (args.includes("--watch")) {
-      // Build and watch extension and webview code
-      console.log("[watch] build started");
-      await build({
-        ...extensionConfig,
-        ...watchConfig,
-      });
-      await build({
-        ...summaryConfig,
-        ...watchConfig,
-      });
-      await build({
-        ...chatConfig,
-        ...watchConfig
-      });
-      await build({
-        ...startConfig,
-        ...watchConfig
-      });
-      await build({
-        ...markdownConfig,
-        ...watchConfig
-      });
-      console.log("[watch] build finished");
-    } else {
-      // Build extension and webview code
-      await build(extensionConfig);
-      await build(summaryConfig);
-      await build(chatConfig);
-      await build(startConfig);
-      await build(markdownConfig);
-      
-      console.log("build complete");
-    }
-  } catch (err) {
-    process.stderr.write(err.stderr);
-    process.exit(1);
-  }
+    const args = process.argv.slice(2);
+        if (args.includes("--watch")) {
+            try {
+                // Build and watch extension and webview code
+                console.log("[WATCH] build started");
+                await build({
+                    ...extensionConfig,
+                    ...watchConfig,
+                });
+                await build({
+                    ...summaryConfig,
+                    ...watchConfig,
+                });
+                await build({
+                    ...chatConfig,
+                    ...watchConfig,
+                });
+                await build({
+                    ...startConfig,
+                    ...watchConfig,
+                });
+                await build({
+                    ...markdownConfig,
+                    ...watchConfig,
+                });
+                console.log("[WATCH] build successfully");
+            } catch (err) {
+                console.log("[WATCH] build failed with an exception");
+                console.error(err.message || err);
+                process.exit(1);
+            } finally {
+                console.log("[WATCH] build script completed");
+            }
+        } else {
+            try {
+                // Build and watch extension and webview code
+                console.log("[FULL] build started");
+                // Build extension and webview code
+                await build(extensionConfig);
+                await build(summaryConfig);
+                await build(chatConfig);
+                await build(startConfig);
+                await build(markdownConfig);
+                console.log("[FULL] build successfully");
+            } catch (err) {
+                console.log("[FULL] build failed with an exception");
+                console.error(err.message || err);
+                process.exit(1);
+            } finally {
+                console.log("[FULL] build script completed");
+            }
+        }
 })();
