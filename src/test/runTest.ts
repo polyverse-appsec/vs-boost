005c9ea1cae5ece91fd1986315ff7c7b5e730079
Alexander Gounares <alex@gounares.com>
2023-07-23 11:00:06 -0700

    get unit tests hooked into runner, enable all tests in runner

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,51 +1,90 @@
-import * as path from 'path';
-import * as fs from 'fs';
+import * as path from "path";
+import * as fs from "fs";
 
-import { runTests } from '@vscode/test-electron';
+import { runTests } from "@vscode/test-electron";
 
-async function main(argv : string[]) {
+import Mocha from "mocha";
 
-	try {
-        const filenames = argv[2]?.split(",",1);
-        let filename : string | undefined = filenames?filenames[0]:undefined;
+async function runMochaTests() {
+    // Create a Mocha instance
+    console.log("Running Mocha tests");
+    const mocha = new Mocha();
 
-        let targetTestInputPath : string | undefined = undefined;
-        try
-        {
+    // Add test files
+    const testDir = path.resolve(__dirname, "./unit");
+    fs.readdirSync(testDir)
+        .filter((file) => file.endsWith(".test.js"))
+        .forEach((file) => {
+            mocha.addFile(path.join(testDir, file));
+        });
+
+    // Run Mocha tests
+    try {
+        const failures: number = await new Promise((resolve) =>
+            mocha.run(resolve)
+        );
+        if (failures > 0) {
+            throw new Error(`${failures} tests failed.`);
+        }
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+async function main(argv: string[]) {
+    try {
+        // Run Mocha tests
+        await runMochaTests();
+
+        const filenames = argv[2]?.split(",", 1);
+        let filename: string | undefined = filenames ? filenames[0] : undefined;
+
+        let targetTestInputPath: string | undefined = undefined;
+        try {
             if (filename) {
-                console.log('Target Test filename:', filename);
+                console.log("Target Test filename:", filename);
                 const targetTestInput = { filename };
-                targetTestInputPath = path.resolve(__dirname, '../test/resources', 'targetTestInput.json');
-                fs.writeFileSync(targetTestInputPath, JSON.stringify(targetTestInput, null, 2));
+                targetTestInputPath = path.resolve(
+                    __dirname,
+                    "../test/resources",
+                    "targetTestInput.json"
+                );
+                fs.writeFileSync(
+                    targetTestInputPath,
+                    JSON.stringify(targetTestInput, null, 2)
+                );
                 console.log(`Created ${targetTestInputPath}`);
             }
 
             // The folder containing the Extension Manifest package.json
             // Passed to `--extensionDevelopmentPath`
-            const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+            const extensionDevelopmentPath = path.resolve(__dirname, "../../");
 
             // The path to test runner
             // Passed to --extensionTestsPath
-            const extensionTestsPath = path.resolve(__dirname, './suite/index');
+            const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
             // Download VS Code, unzip it and run the integration test
-            let args = [ ];
+            let args = [];
             if (filename) {
                 args.push(filename);
             }
-            await runTests( { extensionDevelopmentPath : extensionDevelopmentPath, extensionTestsPath : extensionTestsPath,
-                launchArgs: args });
-
+            await runTests({
+                extensionDevelopmentPath: extensionDevelopmentPath,
+                extensionTestsPath: extensionTestsPath,
+                launchArgs: args,
+            });
         } finally {
             if (targetTestInputPath) {
                 fs.unlinkSync(targetTestInputPath);
                 console.log(`Deleted ${targetTestInputPath}`);
             }
         }
-	} catch (err) {
-		console.error('Failed to run tests', err);
-		process.exit(1);
-	}
+    } catch (err) {
+        console.error("Failed to run tests", err);
+        process.exit(1);
+    }
 }
 
 main(process.argv);
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,59 +1,67 @@
-import path from 'path';
-import glob from 'glob';
-import fs from 'fs';
-import Mocha, { test } from 'mocha';
-import { shuffle } from 'lodash';
+import path from "path";
+import glob from "glob";
+import fs from "fs";
+import Mocha, { test } from "mocha";
+import { shuffle } from "lodash";
 
 // specify tests to run
-// const testFilter = '**/*.test.js';
-const testFilter = '**/boostdata.test.js';
+//const testFilter = "**/*.test.js";
+//const testFilter = '**/boostdata.test.js';
+// just run tests in this directory, unit tests are run direclty by mocha
+const testFilter = "suite/*.test.js";
 
 export function run(): Promise<void> {
-	const mocha = new Mocha({
-		ui: 'tdd',
-		color: true
-	});
+    const mocha = new Mocha({
+        ui: "tdd",
+        color: true,
+    });
 
-	const testsRoot = path.resolve(__dirname, '..');
+    const testsRoot = path.resolve(__dirname, "..");
 
-	// Check if targetTestInput.json exists and read it
-	const targetTestInputPath = path.resolve(__dirname, '../resources', 'targetTestInput.json');
-	let targetTestFilename: string = '';
-	if (fs.existsSync(targetTestInputPath)) {
-	    const targetTestInput = JSON.parse(fs.readFileSync(targetTestInputPath, 'utf8'));
-	    targetTestFilename = targetTestInput.filename;
-	}
+    // Check if targetTestInput.json exists and read it
+    const targetTestInputPath = path.resolve(
+        __dirname,
+        "../resources",
+        "targetTestInput.json"
+    );
+    let targetTestFilename: string = "";
+    if (fs.existsSync(targetTestInputPath)) {
+        const targetTestInput = JSON.parse(
+            fs.readFileSync(targetTestInputPath, "utf8")
+        );
+        targetTestFilename = targetTestInput.filename;
+    }
 
-	return new Promise((resolve, reject) => {
-		glob(testFilter, { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return reject(err);
-			}
+    return new Promise((resolve, reject) => {
+        glob(testFilter, { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return reject(err);
+            }
 
-			// Shuffle the files so we don't have any ordering effects in tests
-			const shuffledFiles = shuffle(files);
+            // Shuffle the files so we don't have any ordering effects in tests
+            const shuffledFiles = shuffle(files);
 
-			// Add files in randomized order to the test suite
-			shuffledFiles.forEach((file: string) => {
-				if (!targetTestFilename || file.includes(targetTestFilename)) {
-					console.log('Adding Test file:', file);
-					mocha.addFile(path.resolve(testsRoot, file));
-				}
-			});
+            // Add files in randomized order to the test suite
+            shuffledFiles.forEach((file: string) => {
+                if (!targetTestFilename || file.includes(targetTestFilename)) {
+                    console.log("Adding Test file:", file);
+                    mocha.addFile(path.resolve(testsRoot, file));
+                }
+            });
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						reject(new Error(`${failures} tests failed.`));
-					} else {
-						resolve();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				reject(err);
-			}
-		});
-	});
+            try {
+                // Run the mocha test
+                mocha.run((failures) => {
+                    if (failures > 0) {
+                        reject(new Error(`${failures} tests failed.`));
+                    } else {
+                        resolve();
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+                reject(err);
+            }
+        });
+    });
 }
