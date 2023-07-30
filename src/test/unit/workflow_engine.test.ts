704747183b6ba6cab61e966b45763b2e13b89bd2
Alexander Gounares <alex@gounares.com>
2023-07-30 00:27:10 -0700

    add a workflow engine class

---
+++ src/test/unit/workflow_engine.test.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import { WorkflowEngine } from '../../workflow_engine';
+
+describe('WorkflowEngine', () => {
+    it('should run promises in the correct order', async () => {
+        let log: string[] = [];
+
+        const beforeGen = [() => async () => { log.push('before'); return; }] ;
+        const main = [
+            () => async () => { log.push('main1'); return; },
+            () => async () => { log.push('main2'); return; },
+            () => async () => { log.push('main3'); return; },
+            () => async () => { log.push('main4'); return; }
+        ];
+        const afterEachGroup = [() => async () => { log.push('afterEachGroup'); return; }];
+        const afterGen = [() => async () => { log.push('after'); return; }];
+        
+         
+        const pattern = [1, 2];
+
+        const engine = new WorkflowEngine(beforeGen, main, afterEachGroup, afterGen, pattern);
+        await engine.run();
+
+        expect(log).to.deep.equal(['before', 'main1', 'afterEachGroup', 'main2', 'main3', 'afterEachGroup', 'main4', 'afterEachGroup', 'after']);
+    });
+
+    // More tests can be written to verify other functionalities...
+});
+
---
+++ src/workflow_engine.ts
@@ -0,0 +1,99 @@
+/*
+
+the basic idea is the engine will take an array of promise generators, and run through them one at a time in serial until 
+every promise is successfully resolved.  if a promise fails, then the promise generator is re-run and a new promise is put on the queue.  
+
+the special behavior is that the main array of promises is to be run in groups, as specified by a pattern array.  
+thus, if the pattern array is [1, 2, 4, 8], then the first promise is run, followed by *all* of the afterEachGroup promises, 
+then two promises from the main array are run, followed by *all* of the afterEachGroup promises, then four promises are run and so forth.\
+
+Initialization: The engine will be initialized with the following arrays of promise generators:
+    before: Run at the very beginning of the workflow.
+    main: The main group of promise generators.
+    afterEachGroup: Run after every group, as specified by the pattern.
+    after: Run at the end of the workflow.
+    pattern: Specifies the grouping for the main array.
+
+Running the Workflow:
+First, the before promises will be run.
+Then, the main promises will be run in groups specified by the pattern. After each group, all afterEachGroup promises are run.
+If any promise in the main array fails, its generator is re-run, and a new promise is added to the queue.
+Finally, the after promises will be run.
+Abort: If the abort API is called, the workflow will stop executing further promises.
+*/
+
+export type PromiseGenerator = () => () => Promise<any>;
+export type BeforePromiseGenerator = PromiseGenerator;
+export type AfterEachGroupPromiseGenerator = PromiseGenerator;
+export type AfterPromiseGenerator = PromiseGenerator;
+
+export class WorkflowEngine {
+    private before: BeforePromiseGenerator[];
+    private main: PromiseGenerator[];
+    private afterEachGroup: AfterEachGroupPromiseGenerator[];
+    private after: AfterPromiseGenerator[];
+    private pattern: number[];
+    private aborted: boolean = false;
+
+    constructor(
+        before: PromiseGenerator[] | undefined = undefined,
+        main: PromiseGenerator[],
+        afterEachGroup: PromiseGenerator[] | undefined = undefined,
+        after: PromiseGenerator[] | undefined = undefined,
+        pattern: number[] | undefined = undefined
+    ) {
+        this.before = before? before : [];
+        this.main = [...main];
+        this.afterEachGroup = afterEachGroup? afterEachGroup : [];
+        this.after = after? after : [];
+        this.pattern = pattern ? pattern : [1, 2, 4, 8, 16];
+    }
+
+    public async run() {
+        this.aborted = false;
+        await this.executePromises(this.before);
+
+        let groupIndex = 0;
+        while (this.main.length > 0 && !this.aborted) {
+            const groupSize = this.pattern[groupIndex] || this.pattern[this.pattern.length - 1]; // Use the last group size if we've exceeded the pattern
+        
+            for (let i = 0; i < groupSize && this.main.length > 0; i++) {
+                if (this.aborted) {
+                    return;
+                }
+                const promiseGenerator = this.main.shift()!;
+                const promise = promiseGenerator();
+                try {
+                    await promise();
+                } catch (error) {
+                    this.main.push(promiseGenerator); // Retry later
+                }
+            }
+        
+            await this.executePromises(this.afterEachGroup);
+        
+            // Move to the next group size if available
+            if (groupIndex < this.pattern.length - 1) {
+                groupIndex++;
+            }
+        }
+
+        await this.executePromises(this.after);
+    }
+
+    public abort() {
+        this.aborted = true;
+    }
+
+    private async executePromises(promiseGenerators: PromiseGenerator[]) {
+        for (let generatorFactory of promiseGenerators) {
+            if (this.aborted){
+                return;
+            }
+            const generator = generatorFactory();
+            await generator();
+        }
+    }
+    
+}
+
