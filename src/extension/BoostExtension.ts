a7718bf39f11f977ea37a2a58957bd4730543db3
Alexander Gounares <alex@gounares.com>
2023-08-31 10:05:03 -0700

    placeholder to get inline api's working

--- src/extension/BoostExtension.ts
+++ src/extension/BoostExtension.ts
@@ -140,6 +140,7 @@ import {
     BoostChatKernel, chatKernelName,
 } from "../controllers/chat_controller";
 
+import { CodelensProvider } from "../inline/CodelensProvider";
 export class BoostNotebookContentProvider
     implements vscode.TextDocumentContentProvider
 {
@@ -192,6 +193,8 @@ export class BoostExtension {
     private _accountInfo: any | undefined;
 
     public _context: vscode.ExtensionContext | undefined;
+    public codeLensProvider: CodelensProvider | undefined;
+
     problems: vscode.DiagnosticCollection;
 
     successfullyActivated = false;
@@ -250,6 +253,8 @@ export class BoostExtension {
             this.registerSourceCodeRightClickCommands(context);
 
             this.registerShowGuidelinesCommand(context);
+
+            this.registerInlineProviders(context);
 
             this.successfullyActivated = true;
         } catch (e) {
@@ -3786,5 +3791,103 @@ export class BoostExtension {
         }
         // Replace the problems with the updated diagnostics
         problems.set(cellUri, diagnostics);
+    }
+
+    registerInlineProviders(context: vscode.ExtensionContext){
+        this.codeLensProvider = new CodelensProvider();
+        vscode.languages.registerCodeLensProvider("*", this.codeLensProvider);
+        vscode.commands.registerCommand("polyverse-boost-notebook.codelensAction", (args: any) => {
+            vscode.window.showInformationMessage(`CodeLens action clicked with args=${args}`);
+        });
+
+        let timeout: NodeJS.Timer | undefined = undefined;
+
+
+        // create a decorator type that we use to decorate small numbers
+        const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
+            borderWidth: '1px',
+            borderStyle: 'solid',
+            overviewRulerColor: 'blue',
+            overviewRulerLane: vscode.OverviewRulerLane.Right,
+            light: {
+                // this color will be used in light color themes
+                borderColor: 'darkblue'
+            },
+            dark: {
+                // this color will be used in dark color themes
+                borderColor: 'lightblue'
+            }
+        });
+
+        // create a decorator type that we use to decorate large numbers
+        const largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
+            cursor: 'crosshair',
+            // use a themable color. See package.json for the declaration and default values.
+            backgroundColor: { id: 'myextension.largeNumberBackground' }
+        });
+
+        let activeEditor = vscode.window.activeTextEditor;
+
+        function updateDecorations() {
+            if (!activeEditor) {
+                return;
+            }
+            const regEx = /\d+/g;
+            const text = activeEditor.document.getText();
+            const smallNumbers: vscode.DecorationOptions[] = [];
+            const largeNumbers: vscode.DecorationOptions[] = [];
+            const eolDecoration: vscode.DecorationOptions[] = [];
+            
+            let match;
+            while ((match = regEx.exec(text))) {
+                const startPos = activeEditor.document.positionAt(match.index);
+                const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+                const decoration = { 
+                    range: new vscode.Range(startPos, endPos), 
+                    hoverMessage: 'Number **' + match[0] + '**',
+                    renderOptions: {
+                        after: {
+                            contentText: "hello from Boost",
+                        }
+                    }
+                };
+                if (match[0].length < 3) {
+                    smallNumbers.push(decoration);
+                } else {
+                    largeNumbers.push(decoration);
+                }
+            }
+            activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
+            activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
+        }
+
+        function triggerUpdateDecorations(throttle = false) {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = undefined;
+            }
+            if (throttle) {
+                timeout = setTimeout(updateDecorations, 500);
+            } else {
+                updateDecorations();
+            }
+        }
+
+        if (activeEditor) {
+            triggerUpdateDecorations();
+        }
+
+        vscode.window.onDidChangeActiveTextEditor(editor => {
+            activeEditor = editor;
+            if (editor) {
+                triggerUpdateDecorations();
+            }
+        }, null, context.subscriptions);
+
+        vscode.workspace.onDidChangeTextDocument(event => {
+            if (activeEditor && event.document === activeEditor.document) {
+                triggerUpdateDecorations(true);
+            }
+        }, null, context.subscriptions);
     }
 }
