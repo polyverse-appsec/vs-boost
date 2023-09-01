bd96c7bfd10296fc386a0f5b017397523140eb74
Alexander Gounares <alex@gounares.com>
2023-08-31 20:33:57 -0700

    continue refactor and make it depend on the selection

---
+++ src/inline/decorator_provider.ts
@@ -0,0 +1,92 @@
+import * as vscode from 'vscode';
+
+export class DecoratorProvider {
+    private smallNumberDecorationType: vscode.TextEditorDecorationType;
+    private timeout: NodeJS.Timer | undefined = undefined;
+    private activeEditor: vscode.TextEditor | undefined;
+
+    constructor(context: vscode.ExtensionContext) {
+
+        this.smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
+            borderWidth: '1px',
+            borderStyle: 'solid',
+            overviewRulerColor: 'blue',
+            overviewRulerLane: vscode.OverviewRulerLane.Right,
+            light: {
+                borderColor: 'darkblue'
+            },
+            dark: {
+                borderColor: 'lightblue'
+            }
+        });
+
+        this.activeEditor = vscode.window.activeTextEditor;
+
+        this.updateDecorations();
+
+        vscode.window.onDidChangeActiveTextEditor(editor => {
+            this.activeEditor = editor;
+            if (editor) {
+                this.triggerUpdateDecorations();
+            }
+        }, null, context.subscriptions);
+
+        vscode.workspace.onDidChangeTextDocument(event => {
+            if (this.activeEditor && event.document === this.activeEditor.document) {
+                this.triggerUpdateDecorations(true);
+            }
+        }, null, context.subscriptions);
+
+        vscode.window.onDidChangeTextEditorSelection(event => {
+            if (event.textEditor === this.activeEditor) {
+                this.updateDecorations();
+            }
+        }, null, context.subscriptions);
+    }
+
+    private updateDecorations() {
+        if (!this.activeEditor) {
+            return;
+        }
+    
+        const text = this.activeEditor.document.getText();
+        const decorations: vscode.DecorationOptions[] = [];
+    
+        for (const selection of this.activeEditor.selections) {
+            const startLine = selection.start.line;
+            const endLine = selection.end.line;
+    
+            for (let lineNum = startLine; lineNum <= endLine; lineNum++) {
+                const line = this.activeEditor.document.lineAt(lineNum);
+                const startPos = new vscode.Position(lineNum, line.text.length);
+                const endPos = new vscode.Position(lineNum, line.text.length);
+    
+                const decoration = {
+                    range: new vscode.Range(startPos, endPos),
+                    renderOptions: {
+                        after: {
+                            contentText: "hello from Boost",
+                        }
+                    }
+                };
+    
+                decorations.push(decoration);
+            }
+        }
+    
+        this.activeEditor.setDecorations(this.smallNumberDecorationType, decorations);
+    }
+    
+
+    private triggerUpdateDecorations(throttle = false) {
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+            this.timeout = undefined;
+        }
+        if (throttle) {
+            this.timeout = setTimeout(() => this.updateDecorations(), 500);
+        } else {
+            this.updateDecorations();
+        }
+    }
+}
--- src/inline/inline.ts
+++ src/inline/inline.ts
@@ -1,12 +1,10 @@
 import * as vscode from 'vscode';
 import { CodelensProvider } from './codelens_provider';
+import { DecoratorProvider } from './decorator_provider';
 
 export class InlineBoostAnnotations {
-    private codeLensProvider: CodelensProvider;
-    private smallNumberDecorationType: vscode.TextEditorDecorationType;
-    private largeNumberDecorationType: vscode.TextEditorDecorationType;
-    private timeout: NodeJS.Timer | undefined = undefined;
-    private activeEditor: vscode.TextEditor | undefined;
+    public codeLensProvider: CodelensProvider;
+    public decoratorProvider: DecoratorProvider;
 
     constructor(context: vscode.ExtensionContext) {
         this.codeLensProvider = new CodelensProvider();
@@ -15,83 +13,6 @@ export class InlineBoostAnnotations {
             vscode.window.showInformationMessage(`CodeLens action clicked with args=${args}`);
         });
 
-        this.smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            overviewRulerColor: 'blue',
-            overviewRulerLane: vscode.OverviewRulerLane.Right,
-            light: {
-                borderColor: 'darkblue'
-            },
-            dark: {
-                borderColor: 'lightblue'
-            }
-        });
-
-        this.largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
-            cursor: 'crosshair',
-            backgroundColor: { id: 'myextension.largeNumberBackground' }
-        });
-
-        this.activeEditor = vscode.window.activeTextEditor;
-
-        this.updateDecorations();
-
-        vscode.window.onDidChangeActiveTextEditor(editor => {
-            this.activeEditor = editor;
-            if (editor) {
-                this.triggerUpdateDecorations();
-            }
-        }, null, context.subscriptions);
-
-        vscode.workspace.onDidChangeTextDocument(event => {
-            if (this.activeEditor && event.document === this.activeEditor.document) {
-                this.triggerUpdateDecorations(true);
-            }
-        }, null, context.subscriptions);
-    }
-
-    private updateDecorations() {
-        if (!this.activeEditor) {
-            return;
-        }
-        const regEx = /\d+/g;
-        const text = this.activeEditor.document.getText();
-        const smallNumbers: vscode.DecorationOptions[] = [];
-        const largeNumbers: vscode.DecorationOptions[] = [];
-
-        let match;
-        while ((match = regEx.exec(text))) {
-            const startPos = this.activeEditor.document.positionAt(match.index);
-            const endPos = this.activeEditor.document.positionAt(match.index + match[0].length);
-            const decoration = {
-                range: new vscode.Range(startPos, endPos),
-                hoverMessage: 'Number **' + match[0] + '**',
-                renderOptions: {
-                    after: {
-                        contentText: "hello from Boost",
-                    }
-                }
-            };
-            if (match[0].length < 3) {
-                smallNumbers.push(decoration);
-            } else {
-                largeNumbers.push(decoration);
-            }
-        }
-        this.activeEditor.setDecorations(this.smallNumberDecorationType, smallNumbers);
-        this.activeEditor.setDecorations(this.largeNumberDecorationType, largeNumbers);
-    }
-
-    private triggerUpdateDecorations(throttle = false) {
-        if (this.timeout) {
-            clearTimeout(this.timeout);
-            this.timeout = undefined;
-        }
-        if (throttle) {
-            this.timeout = setTimeout(() => this.updateDecorations(), 500);
-        } else {
-            this.updateDecorations();
-        }
+        this.decoratorProvider = new DecoratorProvider(context);
     }
 }
