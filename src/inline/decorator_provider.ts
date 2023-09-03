f76bce7cf9085ea7d6b31db3b7fdd6d0aabbb2d3
Alexander Gounares <alex@gounares.com>
2023-09-02 18:26:59 -0700

    get commands running in the hover view

--- src/inline/decorator_provider.ts
+++ src/inline/decorator_provider.ts
@@ -1,4 +1,4 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export class DecoratorProvider {
     private smallNumberDecorationType: vscode.TextEditorDecorationType;
@@ -6,78 +6,99 @@ export class DecoratorProvider {
     private activeEditor: vscode.TextEditor | undefined;
 
     constructor(context: vscode.ExtensionContext) {
-
-        this.smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            overviewRulerColor: 'blue',
-            overviewRulerLane: vscode.OverviewRulerLane.Right,
-            light: {
-                borderColor: 'darkblue'
-            },
-            dark: {
-                borderColor: 'lightblue'
-            }
-        });
+        this.smallNumberDecorationType =
+            vscode.window.createTextEditorDecorationType({
+                borderWidth: "1px",
+                borderStyle: "solid",
+                overviewRulerColor: "blue",
+                overviewRulerLane: vscode.OverviewRulerLane.Right,
+                light: {
+                    borderColor: "darkblue",
+                },
+                dark: {
+                    borderColor: "lightblue",
+                },
+            });
 
         this.activeEditor = vscode.window.activeTextEditor;
 
         this.updateDecorations();
 
-        vscode.window.onDidChangeActiveTextEditor(editor => {
-            this.activeEditor = editor;
-            if (editor) {
-                this.triggerUpdateDecorations();
-            }
-        }, null, context.subscriptions);
+        vscode.window.onDidChangeActiveTextEditor(
+            (editor) => {
+                this.activeEditor = editor;
+                if (editor) {
+                    this.triggerUpdateDecorations();
+                }
+            },
+            null,
+            context.subscriptions
+        );
 
-        vscode.workspace.onDidChangeTextDocument(event => {
-            if (this.activeEditor && event.document === this.activeEditor.document) {
-                this.triggerUpdateDecorations(true);
-            }
-        }, null, context.subscriptions);
+        vscode.workspace.onDidChangeTextDocument(
+            (event) => {
+                if (
+                    this.activeEditor &&
+                    event.document === this.activeEditor.document
+                ) {
+                    this.triggerUpdateDecorations(true);
+                }
+            },
+            null,
+            context.subscriptions
+        );
 
-        vscode.window.onDidChangeTextEditorSelection(event => {
-            if (event.textEditor === this.activeEditor) {
-                this.updateDecorations();
-            }
-        }, null, context.subscriptions);
+        vscode.window.onDidChangeTextEditorSelection(
+            (event) => {
+                if (event.textEditor === this.activeEditor) {
+                    this.updateDecorations();
+                }
+            },
+            null,
+            context.subscriptions
+        );
     }
 
     private updateDecorations() {
         if (!this.activeEditor) {
             return;
         }
-    
+
         const text = this.activeEditor.document.getText();
         const decorations: vscode.DecorationOptions[] = [];
-    
+
         for (const selection of this.activeEditor.selections) {
             const startLine = selection.start.line;
             const endLine = selection.end.line;
-    
+
             for (let lineNum = startLine; lineNum <= endLine; lineNum++) {
                 const line = this.activeEditor.document.lineAt(lineNum);
                 const startPos = new vscode.Position(lineNum, line.text.length);
                 const endPos = new vscode.Position(lineNum, line.text.length);
-    
+
+                const md = new vscode.MarkdownString(
+                    "[Run command](command:polyverse-boost-notebook.showGuidelines) *Hello* from Boost!"
+                );
+                md.isTrusted = true;
                 const decoration = {
                     range: new vscode.Range(startPos, endPos),
-                    hoverMessage: new vscode.MarkdownString("*Hello* from Boost!"),
+                    hoverMessage: md,
                     renderOptions: {
                         after: {
                             contentText: "hello from Boost",
-                        }
-                    }
+                        },
+                    },
                 };
-    
+
                 decorations.push(decoration);
             }
         }
-    
-        this.activeEditor.setDecorations(this.smallNumberDecorationType, decorations);
+
+        this.activeEditor.setDecorations(
+            this.smallNumberDecorationType,
+            decorations
+        );
     }
-    
 
     private triggerUpdateDecorations(throttle = false) {
         if (this.timeout) {
