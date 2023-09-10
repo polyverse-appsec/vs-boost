f4d92456dafc722b8f79830efd10c43df10d4a14
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-09-09 20:00:46 -0500

    correct fix for empty object

--- src/controllers/function_base_controller.ts
+++ src/controllers/function_base_controller.ts
@@ -88,7 +88,7 @@ export class FunctionKernelControllerBase extends KernelControllerBase {
         notebook: vscode.NotebookDocument | boostnb.BoostNotebook) : any {
 
         //if the details exists, then we will use that as the output as an object
-        if (!details || details.length === 0) {
+        if (!details || Object.keys(details).length === 0) {
             return {};
         }
         //now add the bugs to the issue collection
