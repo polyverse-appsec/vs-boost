992074d52d335e0f0721019ec39b3e09062345f1
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-10-21 14:19:30 -0700

    refactor language mappings for utility reference

--- src/utilities/sourceLoader.ts
+++ src/utilities/sourceLoader.ts
@@ -12,6 +12,7 @@ import {
 
 import { defaultCodeSplitter } from "./languageParsers";
 import { splitCodeWithAggregation } from "./splitWithAggregation";
+import { languageMappings } from "./languageMappings";
 
 function getFileExtension(filename: string): string {
     const lastIndex = filename.lastIndexOf(".");
@@ -24,77 +25,6 @@ function getVSCodeLanguageId(filename: string): string {
         let parsedFilename = path.parse(filename).name;
         fileExtension = parsedFilename || path.basename(filename);
     }
-
-    const languageMappings: { [key: string]: string } = {
-        js: "javascript",
-        ts: "typescript",
-        coffee: "coffeescript",
-        html: "html",
-        vue: "html",
-
-            // Razor support
-        cshtml: "html",
-
-        css: "css",
-        json: "json",
-        xml: "xml",
-        xsl: "xml",
-        xslt: "xml",
-        md: "markdown",
-        py: "python",
-        c: "c",
-        cpp: "cpp",
-        h: "c",
-        hpp: "cpp",
-        cs: "csharp",
-        java: "java",
-        go: "go",
-        rb: "ruby",
-        php: "php",
-        swift: "swift",
-        kt: "kotlin",
-        m: "objective-c",
-        ps1: "powershell",
-        pl: "perl",
-        pm: "perl",
-        pod: "perl",
-        groovy: "groovy",
-        lua: "lua",
-        rs: "rust",
-        sh: "shellscript",
-        bash: "shellscript",
-        r: "r",
-        yml: "yaml",
-        yaml: "yaml",
-        fs: "fsharp",
-        fsx: "fsharp",
-        vb: "vb",
-        txt: "plaintext",
-        sql: "sql",
-        gradle: "plaintext",
-        csproj: "plaintext",
-        vbproj: "plaintext",
-        fsproj: "plaintext",
-        sln: "plaintext",
-        toml: "plaintext",
-        xcodeproj: "plaintext",
-        rakefile: "plaintext",
-        makefile: "plaintext",
-
-        // Salesforce Apex support, we're going to treat as Java for now
-        //  but they're really Apex language files (requiring an Apex extension plugin
-        //  for Visual Studio Code)
-        cls: "java",
-        trigger: "java",
-        object: "java",
-        apex: "java",
-        // Salesforce Visualforce support
-        component: "html",
-        page: "html",
-        // Salesforce Lightning support
-        soql: "sql",
-
-    };
 
     return languageMappings[fileExtension] || "plaintext";
 }
